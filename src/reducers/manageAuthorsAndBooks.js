06a6107d58b7997ff34b8b8dffee440fce17a7fc
Damien de Lartigue <damien@delartigue.co.uk>
2021-04-21 12:53:37 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -12,6 +12,23 @@ function booksReducer(state = [], action){
   switch (action.type) {
     case "ADD_BOOK":
       return [...state, action.book];
-    case 
+    case "REMOVE_BOOK":
+      idx = state.findIndex(book => book.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+    default:
+     return state;
+  }
+}
+
+function authorsReducer(state = [], action){
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(author => author.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+    default:
+     return state;
   }
 }
